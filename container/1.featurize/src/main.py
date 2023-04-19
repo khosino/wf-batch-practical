9f63f7d6086d55bac71f8deb7130c14d022295f3
Kei Hoshino <keihoshino@google.com>
2023-04-19 06:46:31 +0000

    up

--- container/1.featurize/src/main.py
+++ container/1.featurize/src/main.py
@@ -1,22 +1,46 @@
-from sklearn.datasets import fetch_20newsgroups
-from google.cloud import storage
+import os
+import numpy as np
 import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import vstack
+from google.cloud import storage
 
-X_raw, y = fetch_20newsgroups(subset='all', return_X_y=True, shuffle=True, random_state=42, remove=("headers", "footers", "quotes"))  # X is a list of strings and y is a ndarray of category id
+def exec_featurize():
+    # Env
+    print('Start')
+    BUCKET=os.getenv('BUCKET')
+    num_tasks_featurizer=int(os.environ.get('num_tasks_featurizer'))
 
-f = open('X_raw', 'wb')
-pickle.dump(X_raw, f)
-f = open("./X_raw","rb")
-X_raw = pickle.load(f)
+    # Download inputs
+    print(BUCKET)
+    print(num_tasks_featurizer)
+    client= storage.Client()
+    bucket = client.bucket(BUCKET)
+    blob = bucket.blob("input/X_raw")
+    blob.download_to_filename("./X_raw")
 
-f = open('y', 'wb')
-pickle.dump(y, f)
-f = open("./y","rb")
-y = pickle.load(f)
+    f = open("./X_raw","rb")
+    X_raw = pickle.load(f)
 
-client= storage.Client()
-bucket = client.bucket(BUCKET)
-blob = bucket.blob("input/X_raw")
-blob.upload_from_filename("./X_raw")
-blob = bucket.blob("input/y")
-blob.upload_from_filename("./y")
+    # Featurize
+    batch_size = len(X_raw) // num_tasks_featurizer + 1
+    vectorizer = TfidfVectorizer(max_features=300)
+    vectorizer.fit(X_raw)
+    X = []
+    for i in range(10):
+        X.append(vectorizer.transform(X_raw[i * batch_size:(i + 1) * batch_size]))
+    X = vstack(X)  # Convert X from a list of csr_matrix to just a csr_matrix
+
+    # Upload to bucket
+    f = open('X', 'wb')
+    pickle.dump(X, f)
+
+    client= storage.Client()
+    bucket = client.bucket(BUCKET)
+    blob = bucket.blob("featurized/X")
+    blob.upload_from_filename("./X")
+
+    print('complete')
+
+if __name__ == '__main__':
+    exec_featurize()
